cde67718bc973a2d6c821a39b3aa01f940e4dbfb
付羽凡 <5288595+fu_yufan@user.noreply.gitee.com>
2024-06-13 17:04:16 +0800

    feat: lint-staged相关配置

--- src/App.vue
+++ src/App.vue
@@ -16,14 +16,16 @@ import HelloWorld from "./components/HelloWorld.vue"
 
 <style scoped>
 .logo {
-  height: 6em;
-  padding: 1.5em;
   will-change: filter;
+  padding: 1.5em;
+  height: 6em;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
