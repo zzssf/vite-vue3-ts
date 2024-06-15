5bbfa7b7a5593ba7edda19d5f720d417d8b8227d
付羽凡 <5288595+fu_yufan@user.noreply.gitee.com>
2024-06-15 17:10:42 +0800

    feat: 打包生成分析器引入

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import vue from "@vitejs/plugin-vue"
 import { resolve } from "path"
+import { visualizer } from "rollup-plugin-visualizer"
 import AutoImport from "unplugin-auto-import/vite"
 import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
 import Components from "unplugin-vue-components/vite"
@@ -110,6 +111,13 @@ export default ({ command, mode }: ConfigEnv): UserConfigExport => {
       }),
       createStyleImportPlugin({
         resolves: [ElementPlusResolve()]
+      }),
+      visualizer({
+        gzipSize: true,
+        brotliSize: true,
+        emitFile: false,
+        filename: "test.html", //分析图生成的文件名
+        open: true //如果存在本地服务端口，将在打包后自动展示
       })
     ],
     css: {
