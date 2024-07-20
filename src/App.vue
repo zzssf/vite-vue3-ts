47a91ec6f79f4c4a2cfbbb77ae3e45ba9c4b4467
付羽凡 <5288595+fu_yufan@user.noreply.gitee.com>
2024-07-20 15:50:07 +0800

    feat: app.vue页面重构

--- src/App.vue
+++ src/App.vue
@@ -1,37 +1,35 @@
-<script setup lang="ts">
-import HelloWorld from "./components/HelloWorld.vue"
-import styles from "./index.module.scss"
-</script>
-
 <template>
-  <div :class="styles.container">
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <el-config-provider :size="size" :z-index="zIndex" :locale="locale">
+    <router-view />
+  </el-config-provider>
 </template>
 
-<style scoped lang="scss">
-.bg {
-  background-color: $lessBg;
-}
+<script>
+import { defineComponent } from "vue"
+import { ElConfigProvider } from "element-plus"
+import zhCn from "element-plus/lib/locale/lang/zh-cn"
 
-.logo {
-  will-change: filter;
-  padding: 1.5em;
-  height: 6em;
-  transition: filter 300ms;
-}
+export default defineComponent({
+  components: {
+    ElConfigProvider
+  },
+  data() {
+    return {
+      locale: zhCn,
+      zIndex: 3000,
+      size: "default"
+    }
+  }
+})
+</script>
 
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+<style lang="scss">
+html,
+body,
+#app {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
